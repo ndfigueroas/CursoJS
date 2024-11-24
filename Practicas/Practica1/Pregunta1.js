0b76c9455858f2163060d0e0c85b703756587145
ndfigueroas <ndfigueroas@gmail.com>
2024-11-24 11:45:00 -0500

    actualizar nombre de proyectos

---
+++ Practicas/Practica1/Pregunta1.js
@@ -0,0 +1,9 @@
+/* 1. Cálculo de Índice de Masa Corporal (IMC):
+Escribe un programa que pregunte al usuario su peso en kilogramos y su altura en metros. 
+Calcula el Índice de Masa Corporal (IMC) usando la fórmula IMC = peso / (altura * altura) 
+y muestra la categoría correspondiente según el IMC (bajo peso, normal, sobrepeso, obesidad). 
+(10 p)
+*/
+
+
+  
